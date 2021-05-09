88d17ebd410f986c9026b2eea69084c7bbebca29
MdTasib <ohidulalam442@gmail.com>
2021-05-09 17:02:00 +0600

    MongoDb Post products

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = "mongodb+srv://firstCrud:12345ohidul@cluster0.past8.mongodb.net/firstCrud?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
+// get index.html file
 app.get('/', (req, res) => {
-    res.send('working');
+    res.sendFile(__dirname + '/index.html');
 });
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// database connected
 client.connect(err => {
     const collection = client.db("firstCrud").collection("data");
-    console.log('Database connected successfully');
-    client.close();
+
+    // post
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        collection.insertOne(product)
+            .then(result => {
+                console.log('data added successfully');
+                res.send('Success');
+            })
+    });
+
+    // get
+    app.get('/products', (req, res) => {
+        collection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
 });
 
 app.listen(3000);
